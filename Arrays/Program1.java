245ae71b2764ab82fc02478a973a5ef9c637dbb9
developer <chandank.offsureit@gmail.com>
2024-01-04 14:57:47 +0530

    Array-P1-More solutions

--- Arrays/Program1.java
+++ Arrays/Program1.java
@@ -1,5 +1,10 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Program1 {
     static void usingTwoForLoops(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -17,8 +22,52 @@ public class Program1 {
         }
     }
 
+    static void twoPointerMethod(int[] arr) {
+        Arrays.sort(arr);
+
+        int i = 0, j = 0, count = 0;
+
+        while (i < arr.length && j < arr.length) {
+
+            if (arr[i] == arr[j]) {
+                count++;
+                j++;
+            } else {
+                System.out.println(arr[i] + " repeated " + count + " times");
+                count = 0;
+                i = j;
+            }
+        }
+        System.out.println(arr[i] + " repeated " + count + " times");
+
+    }
+
+    static void usingHashMap(int[] arr) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i])) {
+                map.put(arr[i], 1);
+            } else {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+        }
+        Set<Map.Entry<Integer, Integer>> entrySet = map.entrySet();
+        for (Map.Entry<Integer, Integer> m : entrySet) {
+            int key = m.getKey();
+            int value = m.getValue();
+            System.out.println(key + " repeated " + value + " times");
+        }
+
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 3, 6, 7, 18, 9, 10, 5, 11, 12, 13, 14, 11, 16, 17, 18 };
-        usingTwoForLoops(arr);
+        // usingTwoForLoops(arr);
+        System.out.println(":::::::::::::::::::::::::::::::::::::::");
+        twoPointerMethod(arr);
+        System.out.println(":::::::::::::::::::::::::::::::::::::::");
+        usingHashMap(arr);
+
     }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -6,3 +6,11 @@
 # Arrays
 
 1. Java Program to find the frequency of each element in the array
+2. Java Program to left rotate the elements of an array
+3. Java Program to print the duplicate elements of an array
+4. Java Program to print the largest and smallest element in an array
+5. Find the contiguous subarray within an array (containing at least one number) which
+has the largest sum.For example, given the array [−2,1,−3,4,−1,2,1,−5,4], the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+6. Find the contiguous subarray within an array (containing at least one number) which
+has the largest product. For example, given the array [2,3,-2,4], the contiguous subarray [2,3] has the largest product = 6.
+7. 
